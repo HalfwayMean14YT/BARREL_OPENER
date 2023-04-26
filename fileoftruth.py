5d46e2bf49f012c589453127bf5ea14746293cd1
HalfwayMean14YT <131476409+HalfwayMean14YT@users.noreply.github.com>
2023-04-26 11:43:20 -0600

    create a black box

--- fileoftruth.py
+++ fileoftruth.py
@@ -1,1 +1,24 @@
-print("Hello World")
+import pygame
+
+pygame.init()
+
+WIDTH, HEIGHT = 500, 500
+WHITE = (255, 255, 255)
+
+def main():
+    screen = pygame.display.set_mode((WIDTH, HEIGHT))
+    pygame.display.set_caption('Barrel Opener')
+    clock = pygame.time.Clock()
+    background = pygame.Surface(screen.get_size())
+    background.fill(WHITE)
+
+    while True:
+        clock.tick(30)
+        screen.fill(WHITE)
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+
+if __name__ == "__main__":
+    main()
