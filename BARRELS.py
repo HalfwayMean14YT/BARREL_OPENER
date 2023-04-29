cbb45cf8ae4179c7e1e0400982a9a52a8f30d972
HalfwayMean14YT <thomasdalep4@gmail.com>
2023-04-29 03:43:30 -0600

    added crude barrel

--- BARRELS.py
+++ BARRELS.py
@@ -1,9 +1,12 @@
 import pygame
+import os
 
 pygame.init()
 
 WIDTH, HEIGHT = 1000, 1000
 WHITE = (255, 255, 255)
+
+path = os.path.dirname(os.path.abspath(__file__))
 
 def main():
     screen = pygame.display.set_mode((WIDTH, HEIGHT))
@@ -11,33 +14,28 @@ def main():
     clock = pygame.time.Clock()
     background = pygame.Surface(screen.get_size())
     background.fill(WHITE)
-    blackkits = pygame.image.load('blackkits.png')
-    
+    blackkits = pygame.image.load(path + '/images/blackkits.png')
+    barrel = pygame.image.load(path + '/images/barrel.png') 
+    barrel = pygame.transform.scale(barrel, (412,412))
 
-    
-
-    
-    
-    
     def draw_blackkits():
         screen.blit(blackkits, (0,0))
-
+    
+    def draw_barrel():
+        screen.blit(barrel, (0,270))
 
     while True:
         clock.tick(30)
         screen.fill(WHITE)
        
-
         draw_blackkits()
+        draw_barrel()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
         pygame.display.update()
         
-        
-
-    
-      
+          
 if __name__ == "__main__":
     
     main()
