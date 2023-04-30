befae335eb1d45f2e359e0eba37ef18da85d5646
DaenenTDD <44416985+DaenenTDD@users.noreply.github.com>
2023-04-30 04:32:21 -0600

    add scrap with a simple test button

--- BARRELS.py
+++ BARRELS.py
@@ -1,4 +1,5 @@
 import pygame
+import pygame_gui
 import os
 
 pygame.init()
@@ -7,6 +8,8 @@ WIDTH, HEIGHT = 1000, 1000
 WHITE = (255, 255, 255)
 
 path = os.path.dirname(os.path.abspath(__file__))
+
+manager = pygame_gui.UIManager((WIDTH, HEIGHT), path + '/themeing.json', True)
 
 def main():
     screen = pygame.display.set_mode((WIDTH, HEIGHT))
@@ -17,22 +20,43 @@ def main():
     blackkits = pygame.image.load(path + '/images/blackkits.png')
     barrel = pygame.image.load(path + '/images/barrel.png') 
     barrel = pygame.transform.scale(barrel, (412,412))
+    scrap = pygame.image.load(path + '/images/scrap.png')
+    scrap = pygame.transform.scale(scrap, (225,225))
 
     def draw_blackkits():
         screen.blit(blackkits, (0,0))
     
     def draw_barrel():
         screen.blit(barrel, (0,270))
+        
+    def draw_scrap():
+        screen.blit(scrap, (400, 675))
 
+    scrap_button = pygame_gui.elements.UIButton(relative_rect=pygame.Rect((400, 675), (255, 255)),
+                                            text='',
+                                            manager=manager,
+                                            object_id=pygame_gui.core.ObjectID(object_id='#scrap_button'))
     while True:
-        clock.tick(30)
+        time_delta = clock.tick(60)/1000
         screen.fill(WHITE)
        
         draw_blackkits()
         draw_barrel()
+        draw_scrap()
+        
+        
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                pygame.quit()
+                exit()
+                
+            if event.type == pygame_gui.UI_BUTTON_PRESSED:
+                if event.ui_element == scrap_button:
+                    print("test")
+                    
+                
+            manager.process_events(event)
+        manager.update(time_delta)
+        manager.draw_ui(screen)
         pygame.display.update()
         
           
